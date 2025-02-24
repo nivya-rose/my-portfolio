723957560187f43dd33c1db391d83a4c0c468d32
nivya-rose <nivyarose05@gmail.com>
2025-02-24 12:35:35 +0530

    Your commit message

--- staticfiles/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const responseMessage = document.getElementById('response-message');
-    responseMessage.textContent = 'Thank you for your message!';
-    responseMessage.style.color = 'green';
-    this.reset();
-});
